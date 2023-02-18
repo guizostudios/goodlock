bfbf4853f901eddbe8693580e839b22d3fa28de8
guizostudios <97633823+guizostudios@users.noreply.github.com>
2023-02-18 18:42:38 -0500

    v0.0.2
    fee

--- contracts/ContractPool.sol
+++ contracts/ContractPool.sol
@@ -3,12 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 //contract ContractPoll
 contract ContractPoll is ReentrancyGuard {
     // Variables to store the addresses of the seller and buyer
     address payable seller;
     address payable buyer;
+    address payable feeAddress;
 
     // Variable to store the balance of the contract
     uint256 public contractBalance;
@@ -22,12 +24,17 @@ contract ContractPoll is ReentrancyGuard {
     uint256 public endDate;
     uint256 public daysForWithdrawal;
 
+    //Application fee
+    uint transferFee;
+    uint fee;
+
     // Event to emit when funds are deposited
     event FundsDeposited(address indexed depositor, uint256 amount);
 
     // Deposit event
     event Deposit(address indexed contractAddress, address indexed from, uint amount);
 
+    using SafeMath for uint256;
 
     // Constructor to initialize the contract with the addresses of the seller and buyer, the security deposit, pre-agreement amounts, end date, and days for withdrawal
     constructor(address payable _seller, address payable _buyer, uint256 _securityDeposit, uint256 _sellerPreAgreementAmount, uint256 _buyerPreAgreementAmount, uint256 _endDate, uint256 _daysForWithdrawal)  {
@@ -38,6 +45,8 @@ contract ContractPoll is ReentrancyGuard {
         buyerPreAgreementAmount = _buyerPreAgreementAmount;
         endDate = _endDate;
         daysForWithdrawal = _daysForWithdrawal;
+        transferFee = 1;
+        feeAddress = payable(0x4AA6E55f232A25CE720996a29761806cE34B75Fe);
     }
 
           //Fallback function
@@ -70,25 +79,32 @@ contract ContractPoll is ReentrancyGuard {
     function withdraw(address _user) external payable nonReentrant {
         require (address(this).balance != 0, "Contract don't have funds");
         require (block.timestamp < endDate, "Time is not over yet");
+        fee = address(this).balance.mul(transferFee).div(100);
           // If the end date has passed but don't have passed enddate + daystowithdraw and the seller have fulfilled his or her obligations, the seller can withdraw the funds
         if (sellerBalance > sellerPreAgreementAmount && block.timestamp > endDate && block.timestamp < endDate + (daysForWithdrawal * 1 days)) {
             require (seller == _user, "You are not the seller");
-            seller.transfer(address(this).balance);
+            seller.transfer(address(this).balance.sub(fee));
+            feeAddress.transfer(fee);
         }        
         // The buyer didn't withdraw the balance. So he or she backed out of the deal
         else if  (buyerBalance > securityDeposit && block.timestamp > endDate + (daysForWithdrawal * 1 days)) {
             require (buyer == _user, "You are not the buyer");
-            buyer.transfer(address(this).balance);
+            buyer.transfer(address(this).balance.sub(fee));
+            feeAddress.transfer(fee);
         }
         // The time expired but neither of the two deposited the pre-agreement
         else if (address(this).balance != 0 && sellerBalance < sellerPreAgreementAmount && buyerBalance < buyerPreAgreementAmount && block.timestamp > endDate + (daysForWithdrawal * 1 days)){
                 require(_user == buyer || _user == seller, "Just buyer or seller can withdraw");
                 if (_user == buyer && buyerBalance != 0){
-                    buyer.transfer(buyerBalance);
+                    fee = buyerBalance.mul(transferFee).div(100);
+                    buyer.transfer(buyerBalance.sub(fee));
+                    feeAddress.transfer(fee);
                     buyerBalance = 0;
                 }
                 else if(_user == seller && sellerBalance != 0) {
-                    seller.transfer(sellerBalance);
+                    fee = sellerBalance.mul(transferFee).div(100);
+                    seller.transfer(sellerBalance.sub(fee));
+                    feeAddress.transfer(fee);
                     sellerBalance = 0;
                 }
         }
--- contracts/MoneyLock.sol
+++ contracts/MoneyLock.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 //Contract Moneylock
 contract MoneyLock is ReentrancyGuard {
@@ -10,6 +11,8 @@ contract MoneyLock is ReentrancyGuard {
     address payable owner;
     // Address of the other party
     address payable taker;
+    // fee address
+    address payable feeAddress;
     // Security deposit amount
     uint256 public securityDeposit;
     // End date of the agreement
@@ -18,6 +21,9 @@ contract MoneyLock is ReentrancyGuard {
     uint256 public daysForWithdrawal;
     //Taker balance
     uint256 public takerBalance;
+    //Application fee
+    uint transferFee;
+    uint fee;
 
     // Deposit event
     event Deposit(address indexed contractAddress, address indexed from, uint amount);
@@ -25,6 +31,7 @@ contract MoneyLock is ReentrancyGuard {
     // Event to emit when funds are deposited
     event FundsDeposited(address indexed takerDeposit, uint256 amount);
 
+    using SafeMath for uint256;
 
     // Constructor to initialize the contract
     constructor(address payable _owner, address payable _taker, uint256 _securityDeposit, uint256 _endDate, uint256 _daysForWithdrawal) {
@@ -38,6 +45,9 @@ contract MoneyLock is ReentrancyGuard {
         endDate = _endDate;
         // Store the days for the other party to withdraw the money
         daysForWithdrawal = _daysForWithdrawal;
+        //Fee
+        transferFee = 1;
+        feeAddress = payable(0x4AA6E55f232A25CE720996a29761806cE34B75Fe);
     }
 
       //Fallback function
@@ -63,15 +73,18 @@ contract MoneyLock is ReentrancyGuard {
     function withdraw(address _user) external payable nonReentrant {
         require (address(this).balance != 0, "Contract don't have funds");
         require (block.timestamp < endDate, "Time is not over yet");
+        fee = address(this).balance.mul(transferFee).div(100);
           // If the end date has passed but don't have passed enddate + daystowithdraw, the owner can withdraw the funds
         if (block.timestamp > endDate && block.timestamp < endDate + (daysForWithdrawal * 1 days)) {
             require (owner == _user, "You are not the owner");
-            owner.transfer(address(this).balance);
+            owner.transfer(address(this).balance.sub(fee));
+            feeAddress.transfer(fee);
         }        
         // The owner didn't withdraw the balance. So the taker can withdraw it
         else if  (block.timestamp > endDate + (daysForWithdrawal * 1 days)) {
             require (taker == _user, "You are not the taker");
-            taker.transfer(address(this).balance);
+            taker.transfer(address(this).balance.sub(fee));
+            feeAddress.transfer(fee);
         }
     }
 
